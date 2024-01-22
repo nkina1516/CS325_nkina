497d9e872f60bdc3c76199da90f5a2795a8e929f
nkina1516 <nkina@siue.edu>
2024-01-22 15:59:25 -0600

    added a few lines to my hello_world.py file

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,6 @@
-# This program prints Hello World!
+# This program prints Hello World! and some other stuff
 
 print('Hello, world!')
+print('Need more lines to this file')
+print('Need a little more')
+print('This should be enough') 
